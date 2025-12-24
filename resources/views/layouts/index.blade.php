d247cbbd52bcfdb2381da93c27794533990a12af
Ronny Febrian Saputra <ronnyfebriansaputra04@gmail.com>
2025-12-24 14:11:58 +0700

    update layout

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -3,16 +3,11 @@
 
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>Kaiadmin - Bootstrap 5 Admin Dashboard</title>
-    <meta
-        content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
-        name="viewport" />
-    <link
-        rel="icon"
-        href="{{ asset('assets/img/kaiadmin/favicon.ico') }}"
-        type="image/x-icon" />
+    <title>Monitoring Keuangan - Pusdatin Obat dan Makanan</title>
+    <meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport" />
 
-    <!-- Fonts and icons -->
+    <link rel="icon" href="{{ asset('assets/img/kaiadmin/favicon.ico') }}" type="image/x-icon" />
+
     <script src="{{ asset('assets/js/plugin/webfont/webfont.min.js') }}"></script>
     <script>
         WebFont.load({
@@ -34,21 +29,18 @@
         });
     </script>
 
-    <!-- CSS Files -->
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/plugins.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/kaiadmin.min.css') }}" />
 
-    <!-- CSS Just for demo purpose, don't include it in your project -->
+    @stack('styles')
+
     <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}" />
 </head>
 
 <body>
     <div class="wrapper">
-        <!-- Sidebar -->
         @include('layouts.sidebar')
-        <!-- End Sidebar -->
-
         <div class="main-panel">
             @include('layouts.header')
 
@@ -63,39 +55,22 @@
             @include('layouts.footer')
         </div>
     </div>
-    <!--   Core JS Files   -->
+
     <script src="{{ asset('assets/js/core/jquery-3.7.1.min.js') }}"></script>
     <script src="{{ asset('assets/js/core/popper.min.js') }}"></script>
     <script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
 
-    <!-- jQuery Scrollbar -->
     <script src="{{ asset('assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js') }}"></script>
 
-    <!-- Chart JS -->
     <script src="{{ asset('assets/js/plugin/chart.js/chart.min.js') }}"></script>
-
-    <!-- jQuery Sparkline -->
     <script src="{{ asset('assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js') }}"></script>
-
-    <!-- Chart Circle -->
     <script src="{{ asset('assets/js/plugin/chart-circle/circles.min.js') }}"></script>
 
-    <!-- Datatables -->
     <script src="{{ asset('assets/js/plugin/datatables/datatables.min.js') }}"></script>
 
-    <!-- Bootstrap Notify -->
     <script src="{{ asset('assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js') }}"></script>
-
-    <!-- jQuery Vector Maps -->
-    <script src="{{ asset('assets/js/plugin/jsvectormap/jsvectormap.min.js') }}"></script>
-    <script src="{{ asset('assets/js/plugin/jsvectormap/world.js') }}"></script>
-    <!-- Google Maps Plugin -->
-    <script src="{{ asset('assets/js/plugin/gmaps/gmaps.js') }}"></script>
-
-    <!-- Sweet Alert -->
     <script src="{{ asset('assets/js/plugin/sweetalert/sweetalert.min.js') }}"></script>
 
-    <!-- Kaiadmin JS -->
     <script src="{{ asset('assets/js/kaiadmin.min.js') }}"></script>
 
     @stack('scripts')
